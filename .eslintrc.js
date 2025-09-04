2141c7e0c3a7a6de4640514c9ad98013f413c636
kishore <shimikeri.kishore@gmail.com>
2025-09-03 23:50:44 -0700

    Fix lint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'off',
+    'no-redeclare': 'off', // Allow type and const with same name
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    '*.js',
+  ],
+};
